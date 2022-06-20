0d30ed9ff9bae8181d785965f8524e1c3ab17130
taratichenkodmitri <tarataichenkodmitri@gmail.com>
2022-06-20 17:07:32 +0600

    feat: add type to app.js

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,17 +1,27 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 
-class App {
+interface IApp {
+    controller: AppController;
+    view: AppView;
+}
+
+class App implements IApp {
+    public controller;
+    public view;
+
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
     }
 
-    start() {
-        document
-            .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
-        this.controller.getSources((data) => this.view.drawSources(data));
+    start(): void {
+        const sources: HTMLDivElement | null = document.querySelector('.sources');
+
+        if (sources) {
+            sources.addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
+            this.controller.getSources((data) => this.view.drawSources(data));
+        }
     }
 }
 
