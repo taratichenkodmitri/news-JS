8071ba2430fabbbede77a83a5cb02f3acc80a0c9
taratichenkodmitri <tarataichenkodmitri@gmail.com>
2022-06-20 17:38:10 +0600

    feat: add type to loader.ts

--- src/components/controller/loader.ts
+++ src/components/controller/loader.ts
@@ -1,19 +1,32 @@
-class Loader {
-    constructor(baseLink, options) {
+interface ILoader {
+    baseLink: string;
+    options?: {};
+}
+
+interface IURL {
+    endpoint: string;
+    options?: object;
+}
+
+class Loader implements ILoader {
+    public baseLink: string;
+    public options?: {};
+
+    constructor(baseLink: string, options?: {}) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
     getResp(
-        { endpoint, options = {} },
+        { endpoint, options = {} }: IURL,
         callback = () => {
             console.error('No callback for GET response');
         }
-    ) {
+    ): void {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    errorHandler(res: Response): Response {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,9 +36,9 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
-        const urlOptions = { ...this.options, ...options };
-        let url = `${this.baseLink}${endpoint}?`;
+    makeUrl(options: object, endpoint: string): string {
+        const urlOptions: object = { ...this.options, ...options };
+        let url: string = `${this.baseLink}${endpoint}?`;
 
         Object.keys(urlOptions).forEach((key) => {
             url += `${key}=${urlOptions[key]}&`;
@@ -34,10 +47,10 @@ class Loader {
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    load(method: string, endpoint: string, callback: Function, options: {} = {}) {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
-            .then((res) => res.json())
+            .then((res: Response) => res.json())
             .then((data) => callback(data))
             .catch((err) => console.error(err));
     }
