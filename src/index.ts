66e8a394e21d2cbde94885bf463d491585560500
taratichenkodmitri <tarataichenkodmitri@gmail.com>
2022-06-20 11:30:20 +0600

    feat: add eslint and webpack config for ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import App from './components/app/app';
 import './global.css';
 
+function print(msg: string) {
+    console.log(msg);
+}
+
 const app = new App();
 app.start();
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index.ts'),
+    devtool: "inline-source-map",
     mode: 'development',
     module: {
         rules: [
@@ -12,14 +13,19 @@ const baseConfig = {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.ts?$/,
+                use: ['ts-loader'],
+                exclude: /node_modules/,
+            },
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['ts', '.js'],
     },
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, '../dist'),
+        path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
         new HtmlWebpackPlugin({
