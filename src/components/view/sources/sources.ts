17ad079b7f1259b4a1414972ce386a2dd560dba3
taratichenkodmitri <tarataichenkodmitri@gmail.com>
2022-06-21 11:28:33 +0600

    feat: add type to sources.ts

--- src/components/view/sources/sources.ts
+++ src/components/view/sources/sources.ts
@@ -2,19 +2,23 @@ import './sources.css';
 
 class Sources {
     draw(data) {
-        const fragment = document.createDocumentFragment();
-        const sourceItemTemp = document.querySelector('#sourceItemTemp');
+        const fragment: DocumentFragment = document.createDocumentFragment();
+        const sourceItemTemp = <HTMLTemplateElement> document.querySelector('#sourceItemTemp');
 
         data.forEach((item) => {
-            const sourceClone = sourceItemTemp.content.cloneNode(true);
+            const sourceClone = <DocumentFragment> sourceItemTemp.content.cloneNode(true);
 
-            sourceClone.querySelector('.source__item-name').textContent = item.name;
-            sourceClone.querySelector('.source__item').setAttribute('data-source-id', item.id);
+            const sourceCloneItemName = <HTMLSpanElement> sourceClone.querySelector('.source__item-name');
+            sourceCloneItemName.textContent = item.name;
+
+            const sourceCloneItem = <HTMLDivElement> sourceClone.querySelector('.source__item-name');
+            sourceCloneItem.setAttribute('data-source-id', item.id);
 
             fragment.append(sourceClone);
         });
 
-        document.querySelector('.sources').append(fragment);
+        const sources = <HTMLDivElement> document.querySelector('.sources');
+        sources.append(fragment);
     }
 }
 
