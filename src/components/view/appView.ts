fe033b6dc29b3fcdb6177708502ff801cffe821d
taratichenkodmitri <tarataichenkodmitri@gmail.com>
2022-06-21 11:17:33 +0600

    feat: add type to loader.ts

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -1,7 +1,15 @@
 import News from './news/news';
 import Sources from './sources/sources';
 
-export class AppView {
+interface IAppView {
+    news: News;
+    sources: Sources;
+}
+
+export class AppView implements IAppView{
+    public news: News;
+    public sources: Sources;
+
     constructor() {
         this.news = new News();
         this.sources = new Sources();
